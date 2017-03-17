f06cd0ca7cd682f5460e2c20ade75f2d90bb773c
Blazej <bwielk@gmail.com>
2017-03-17 19:46:27 +0000

    React revision 2

---
+++ todo/client/src/app.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+var Todo = React.createClass({
+  getInitialState: function(){
+    return {
+      todos: ['wash up', 'nap', 'get some cheese']
+    }
+  },
+
+  render: function(){
+    return(
+      <div id = "todolist">
+        <p>The busiest ppl do that stuff</p>
+        <ul>
+          <li>{this.state.todos[0]}</li>
+          <li>{this.state.todos[1]}</li>
+          <li>{this.state.todos[2]}</li>
+        </ul>
+        <h1>Hello</h1>
+        <p> I have been in a very bad condition</p>
+        <p>{this.props.message}</p>
+        <p><strong>Cheese name</strong> {this.props.cheese.name}</p>
+        <p><strong>smell</strong> {this.props.cheese.smell}</p>
+        <p><strong>price</strong> {this.props.cheese.price}</p>
+      </div>
+      );
+  }
+});
+
+var myCheese = {
+  name: "Camember",
+  smell: "mild",
+  price: "2.30"
+};
+
+window.onload = () => {
+  render(
+    <Todo message = "I like cheese" cheese = {myCheese}/>,
+    document.getElementById('app')
+  )
+}
